e8164c82703688845e9a339e72f838793b9cbda3
mgorem <oremgitonga22@gmail.com>
2022-08-21 11:12:53 +0300

    do signup, firebase not defined error

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,11 +1,25 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
+const app = require("express")();
+
 admin.initializeApp();
 
-const express = require("express");
-const app = express();
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "socialape-f5389.firebaseapp.com",
+  projectId: "socialape-f5389",
+  storageBucket: "socialape-f5389.appspot.com",
+  messagingSenderId: "317069310239",
+  appId: "1:317069310239:web:c404d321fe78fdd4bd182a",
+};
 
+const { initializeApp } = require("firebase/app");
+const fireApp = initializeApp(firebaseConfig);
+
+const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
+
+// function to get documents(screams)
 app.get("/screams", (req, res) => {
   admin
     .firestore()
@@ -27,14 +41,8 @@ app.get("/screams", (req, res) => {
     .catch((err) => console.error(err));
 });
 
-// function for getting documents(screams)
-exports.getScreams = functions.https.onRequest((req, res) => {});
-
 // function for creating documents(screams)
 app.post("/scream", (req, res) => {
-  // if (req.method !== "POST") {
-  //   res.status(400).json({ error: `Method not allowed` }); // error from client
-  // }
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
@@ -54,7 +62,28 @@ app.post("/scream", (req, res) => {
     });
 });
 
-// https://baseurl.com/api/
+// Signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // Todo Validate data
+
+  const auth = getAuth();
+  createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return res
+        .status(201) // Resource created
+        .json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+});
 
 exports.api = functions.https.onRequest(app);
 
