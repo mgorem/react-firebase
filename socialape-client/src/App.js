74292cc140995a8f894d5ed3e69e51b4ad0263ea
mgorem <oremgitonga22@gmail.com>
2022-08-21 15:12:41 +0300

    do themeprovider, fetch data /screams axios 404

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 
 // Components
@@ -10,20 +11,39 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "b22a00",
+      contrastText: "#fff",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- socialape-client/src/pages/Home.jsx
+++ socialape-client/src/pages/Home.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Grid from '@mui/material/Grid'
+import axios from 'axios'
+
+// Components
+import Scream from '../components/Scream'
 
 const Home = () => {
+  const [screams, setScreams] = useState(null)
+
+  useEffect(() => {
+    axios.get('/screams')
+    .then(res => {
+      console.log(res.data)
+      setScreams({
+        screams: res.data
+      })
+    })
+    .catch(err => console.log(err))
+  }, [])
+  
+  let recentScreamsMarkUp = screams ? 
+    screams.map((scream) => <Scream scream={scream} />)
+   : <p style={{fontWeight: 600}}>Ceep Kalm Screams Loading...</p>
+   
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid container spacing={16}>
+      <Grid item sm={8} xs={12}>
+        <p>{recentScreamsMarkUp}</p>
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <p>Profile...</p>
+      </Grid>
+    </Grid>
   )
 }
 
